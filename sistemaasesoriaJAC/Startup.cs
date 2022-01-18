d08762a09b36967de4326303bf96f6fb95de13a6
u20171d221 (Apaza Cueva, Jose Luis) <u20171d221@upc.edu.pe>
2022-01-18 10:08:09 -0500

    Add project files.

---
+++ sistemaasesoriaJAC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sistemaasesoriaJAC.Startup))]
+namespace sistemaasesoriaJAC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
